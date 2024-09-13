f3812fddd4e9345ea6fe8e905db7944d7d85ec17
PlatinumLia <134401063+PlatinumLia@users.noreply.github.com>
2024-09-13 10:39:03 -0300

    Add files via upload

--- modelos/manga.py
+++ modelos/manga.py
@@ -4,15 +4,27 @@ class Manga:
     def __init__(self, nome, volumes):
         self.nome = nome;
         self.volumes = volumes;
-        self.ativo = False;
+        self._ativo = False;
         Manga.mangas.append(self);
 
     def __str__(self):
-        return f'{self.nome} | {self.volumes} | {self.ativo}';
+        return f"{self.nome} | {self.volumes} | {self.ativo}";
 
     def listar_mangas():
+        print("""
+            ██████████████████████████████████████████████████████████████████████▀█████████████
+            █▄─██─▄█▄─▄▄▀██▀▄─██─█─██▀▄─██▄─▄▄▀██▀▄─████▄─▀█▀─▄██▀▄─██▄─▀█▄─▄█─▄▄▄▄██▀▄─██─▄▄▄▄█
+            ██─██─███─▄─▄██─▀─██─▄─██─▀─███─▄─▄██─▀─█████─█▄█─███─▀─███─█▄▀─██─██▄─██─▀─██▄▄▄▄─█
+            ▀▀▄▄▄▄▀▀▄▄▀▄▄▀▄▄▀▄▄▀▄▀▄▀▄▄▀▄▄▀▄▄▀▄▄▀▄▄▀▄▄▀▀▀▄▄▄▀▄▄▄▀▄▄▀▄▄▀▄▄▄▀▀▄▄▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▄▄▀
+              """);
+        print(f"{"Nome do mangá".ljust(2)} | {"Volumes".ljust(2)} | {"Ativo"}");
+
         for manga in Manga.mangas:
-            print(f'{manga.nome} | {manga.volumes}');
+            print(f"{manga.nome.ljust(13)} | {manga.volumes.ljust(7)} | {manga.ativo}");
+
+    @property
+    def ativo(self):
+        return "✘ não ativo" if self._ativo else "✔ ativo";
 
 manga_bleach = Manga("Bleach", "74");
 manga_blackclover = Manga("Black Clover", "36");
