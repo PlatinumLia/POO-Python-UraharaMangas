0e1af8e8f12ef672d343febb972606ec6080fa62
PlatinumLia <134401063+PlatinumLia@users.noreply.github.com>
2024-09-13 07:46:23 -0300

    Add files via upload

--- modelos/manga.py
+++ modelos/manga.py
@@ -1,16 +1,22 @@
 class Manga:
+    mangas = [];
+
     def __init__(self, nome, volumes):
         self.nome = nome;
         self.volumes = volumes;
         self.ativo = False;
+        Manga.mangas.append(self);
 
     def __str__(self):
         return f'{self.nome} | {self.volumes} | {self.ativo}';
+
+    def listar_mangas():
+        for manga in Manga.mangas:
+            print(f'{manga.nome} | {manga.volumes}');
 
 manga_bleach = Manga("Bleach", "74");
 manga_blackclover = Manga("Black Clover", "36");
 
 mangas = [manga_bleach, manga_blackclover];
 
-print(manga_bleach, "\n");
-print(manga_blackclover, "\n");
+Manga.listar_mangas();
